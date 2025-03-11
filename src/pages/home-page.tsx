9b761000a471a34ea10689a9a3c400cdec66d66a
jw-r <evencoding@gmail.com>
2025-03-11 12:23:40 +0900

    feat: asd

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -3,7 +3,7 @@ import { Text } from "@/shared/components/ui/text"
 export const HomePage = () => {
   return (
     <div>
-      <Text typo="h1">asd</Text>
+      <Text typo="h3">PWA혹은 피씨로 접속해줘서 고마워</Text>
     </div>
   )
 }
