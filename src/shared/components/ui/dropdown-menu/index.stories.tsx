b294a1028f659f5e281697c3f8ddbce9df4ae58f
jw-r <evencoding@gmail.com>
2025-03-15 18:53:46 +0900

    fix: story

--- src/shared/components/ui/dropdown-menu/index.stories.tsx
+++ src/shared/components/ui/dropdown-menu/index.stories.tsx
@@ -5,28 +5,29 @@ import { Badge, Check } from "lucide-react"
 const meta: Meta<typeof DropdownMenu> = {
   title: "UI/DropdownMenu",
   component: DropdownMenu,
+  decorators: [
+    (Story) => (
+      <div className="p-[50px]">
+        <Story />
+      </div>
+    ),
+  ],
 }
 export default meta
 
 export const Default: StoryObj<typeof DropdownMenu> = {
-  render: () => <SimpleDropdown />,
-}
-
-function SimpleDropdown() {
-  return (
-    <div style={{ padding: "50px" }}>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <button className="border p-3">Open Menu</button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem right={<Check />}>Item 1</DropdownMenuItem>
-          <DropdownMenuItem right={<Check />}>Item 2</DropdownMenuItem>
-          <DropdownMenuItem color="critical" right={<Badge />}>
-            Item 3
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
-  )
+  render: () => (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="border p-3">Open Menu</button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem right={<Check />}>Item 1</DropdownMenuItem>
+        <DropdownMenuItem right={<Check />}>Item 2</DropdownMenuItem>
+        <DropdownMenuItem color="critical" right={<Badge />}>
+          Item 3
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  ),
 }
