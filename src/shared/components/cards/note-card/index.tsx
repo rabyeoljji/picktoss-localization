29f01f6558add9809bd3e7251bd8a210075c9322
nageuna922@gmail.com <nageuna922@gmail.com>
2025-03-19 00:53:42 +0900

    feat: note card

---
+++ src/shared/components/cards/note-card/index.tsx
@@ -0,0 +1,185 @@
+import { useState } from 'react'
+
+import { PanInfo, motion, useAnimation, useMotionValue } from 'framer-motion'
+
+import { IcFolder } from '@/shared/assets/icon'
+import { Text } from '@/shared/components/ui/text'
+import { useRouter } from '@/shared/lib/router'
+import { cn } from '@/shared/lib/utils'
+
+import { NoteIcon } from '../../bg-icons/note-icon'
+
+interface Props {
+  children: React.ReactNode
+  id: number
+  selectMode: boolean
+  onClick: () => void
+  swipeOptions: React.ReactNode[]
+  className?: HTMLElement['className']
+}
+
+export const NoteCard = ({ children, className, id, selectMode, onClick, swipeOptions }: Props) => {
+  const [isSwiped, setIsSwiped] = useState(false)
+  const [isDragging, setIsDragging] = useState(false)
+  const x = useMotionValue(0)
+  const controls = useAnimation()
+  const router = useRouter()
+
+  // const isChecked = checkDoc.isChecked(id)
+
+  // const handleCheckedChange = (checked: boolean) => {
+  //   if (!selectMode) return
+
+  //   checked ? checkDoc.check(id) : checkDoc.unCheck(id)
+  // }
+
+  const handleDragEnd = async (_: MouseEvent | TouchEvent | PointerEvent, info: PanInfo) => {
+    if (info.offset.x < -30) {
+      setIsSwiped(true) // 30px 이상 드래그하면 스와이프
+      await controls.start({ x: -130 }) // 요소 왼쪽으로 130px 이동
+    } else {
+      setIsSwiped(false) // 스와이프 취소
+      await controls.start({ x: 0 }) // 원래 위치로 이동
+    }
+    setIsDragging(false)
+  }
+
+  // const handleClickCard = () => {
+  //   noteClickEvent()
+
+  //   if (selectMode) {
+  //     handleCheckedChange(!isChecked)
+  //     return
+  //   }
+  //   if (!isDragging && !isSwiped) {
+  //     router.push(`/document/${id}`)
+  //   }
+  // }
+
+  const handleClickCard = () => {
+    onClick()
+
+    if (!selectMode && !isDragging && !isSwiped) {
+      router.push('/note/:noteId', { params: [id] })
+    }
+  }
+
+  return (
+    <div
+      onClick={handleClickCard}
+      className={cn(
+        `relative flex h-[104px] max-w-full items-center overflow-hidden rounded-[16px] bg-white px-[16px] pb-[20px] pt-[17px] shrink-0 cursor-pointer`,
+        className,
+      )}
+    >
+      {/* Swipe 영역 */}
+      <motion.div
+        className="flex h-[104px] max-w-full items-center rounded-[16px] px-[16px] py-[17px]"
+        drag={selectMode ? false : 'x'}
+        dragConstraints={{ left: -130, right: 0 }}
+        onDrag={() => !selectMode && setIsDragging(true)}
+        onDragEnd={handleDragEnd}
+        animate={controls}
+        style={{ x }}
+        transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+      >
+        {children}
+
+        {/* Swipe로 보여지는 버튼 영역 */}
+        <motion.div
+          initial={false}
+          animate={{ x: isSwiped ? 144 : 160 }}
+          transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+          className="absolute inset-y-0 right-0 flex h-[calc(100%+2px)] flex-row overflow-hidden rounded-r-[16px]"
+        >
+          {swipeOptions.map((option) => option)}
+          {/* <MoveDocumentDrawer
+            triggerComponent={
+              <button className="flex-center w-[72px] flex-col rounded-lg bg-background-container-03 p-2 text-text1-medium text-text-info">
+                <Icon name="move" className="mb-[4px]" />
+                이동
+              </button>
+            }
+            documentId={id}
+            resetSwipe={handleResetSwipe}
+          />
+          <DeleteDocumentSwipeButton documentId={id} quizCount={quizCount} /> */}
+        </motion.div>
+      </motion.div>
+    </div>
+  )
+}
+
+const NoteCardLeft = ({
+  type,
+  checkBox,
+  selectMode,
+}: {
+  // type: 'FILE' | 'TEXT' | 'NOTION'
+  type: 'FILE' | 'TEXT'
+  checkBox: React.ReactNode
+  selectMode: boolean
+}) => {
+  return (
+    <>
+      {selectMode ? (
+        <>{checkBox}</>
+      ) : (
+        <NoteIcon type={type} containerClassName="size-[36px]" iconClassName="size-[16px]" />
+      )}
+    </>
+  )
+}
+
+const NoteCardContents = ({ children }: { children: React.ReactNode }) => {
+  return <div className="ml-[16px] flex w-full flex-col">{children}</div>
+}
+
+const NoteCardHeader = ({ title, tag }: { title: string; tag?: React.ReactNode }) => {
+  return (
+    <div className="mb-[2px] flex items-center gap-[8px]">
+      <Text as="h4" typo="subtitle-2-bold" className="w-fit max-w-[calc(100%-100px)] overflow-x-hidden truncate">
+        {title}
+      </Text>
+
+      {tag}
+    </div>
+  )
+}
+
+const NoteCardPreview = ({ content }: { content: string }) => {
+  return (
+    <Text typo="body-1-regular" color="sub" className="w-[calc(100%-55px)] truncate text-nowrap break-all">
+      {content}
+    </Text>
+  )
+}
+
+const NoteCardDetail = ({
+  quizCount,
+  charCount,
+  directory,
+}: {
+  quizCount: number
+  charCount: number
+  directory: string
+}) => {
+  return (
+    <Text typo="body-2-medium" color="sub" className="flex w-fit items-center">
+      <span>{quizCount}문제</span>
+      <div className="inline-block size-fit mx-[8px] text-icon-sub">•</div>
+      <span>{charCount}자</span>
+      <div className="inline-block size-fit mx-[8px] text-icon-sub">•</div>
+      <span className="flex items-center">
+        <IcFolder className="size-[12px] mr-[2px] text-icon-tertiary" />
+        {directory}
+      </span>
+    </Text>
+  )
+}
+
+NoteCard.Left = NoteCardLeft
+NoteCard.Contents = NoteCardContents
+NoteCard.Header = NoteCardHeader
+NoteCard.Preview = NoteCardPreview
+NoteCard.Detail = NoteCardDetail
