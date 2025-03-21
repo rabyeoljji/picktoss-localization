66a16f6c50fcd498d0a0ffa3578fab1f7938e569
jw-r <evencoding@gmail.com>
2025-03-21 16:25:37 +0900

    fix: router

--- src/shared/lib/router/config/index.ts
+++ src/shared/lib/router/config/index.ts
@@ -13,47 +13,49 @@ export const RoutePath = {
   /** 메인 홈 화면 */
   root: '/',
 
-  /** 계정 페이지 */
-  account: '/account',
-
   /** 로그인 페이지 */
   login: '/login',
-
-  /** 게스트 로그인 페이지 */
-  guestLogin: '/guest-login',
-
-  /** 이메일 로그인 페이지 */
-  emailLogin: '/email-login',
-
-  /** 회원가입 페이지 */
-  signUp: '/sign-up',
-
-  /** 랜덤 퀴즈 페이지 */
-  randomQuiz: '/quiz/random',
-
-  /** 밤 퀴즈 페이지 */
-  bombQuiz: '/quiz/bomb',
-
-  /** 메인 퀴즈 페이지 */
-  quiz: '/quiz',
-
-  /** 노트 리스트 페이지 */
-  notes: '/notes',
-
-  /** 노트 상세 페이지 (동적 파라미터: noteId) */
-  note: '/note/:noteId',
-
-  /** 컬렉션 리스트 페이지 */
-  collections: '/collections',
-
-  /** 컬렉션 상세 페이지 (동적 파라미터: collectionId) */
-  collection: '/collection/:collectionId',
-
-  /** 컬렉션 검색 페이지 */
+  
+  /** 설치 가이드 페이지 */
+  installGuide: '/install-guide',
+  
+  /** 노트 관련 */
+  notes: '/note',
+  noteDetail: '/note/:noteId',
+  noteQuiz: '/note/quiz/:noteId',
+  noteArrange: '/note/arrange/:directoryId',
+  noteSearch: '/note/search',
+  noteEdit: '/note/edit/:noteId',
+  noteWrite: '/note/write',
+  noteUpload: '/note/upload',
+  
+  /** 계정 관련 */
+  account: '/account',
+  accountInfo: '/account/info',
+  dailyQuizAttendance: '/account/daily-quiz-attendance',
+  quizAnalysis: '/account/quiz-analysis',
+  quizRecord: '/account/quiz-record',
+  notificationConfig: '/account/notification-config',
+  paymentHistory: '/account/payment-history',
+  notice: '/account/notice',
+  contact: '/account/contact',
+  faq: '/account/faq',
+  policy: '/account/policy',
+  withdraw: '/account/withdraw',
+  
+  /** 테마 퀴즈 */
+  randomQuiz: '/random-quiz',
+  bombQuiz: '/bomb-quiz',
+  
+  /** 컬렉션 관련 */
+  collections: '/collection',
+  collectionDetail: '/collection/:collectionId',
+  collectionQuiz: '/collection/quiz/:collectionId',
+  collectionComplain: '/collection/complain/:collectionId',
+  collectionCreate: '/collection/create',
+  collectionEditInfo: '/collection/edit/info/:collectionId',
+  collectionEditQuiz: '/collection/edit/quiz/:collectionId',
   collectionSearch: '/collection/search',
-
-  /** 설정 페이지 */
-  setting: '/setting',
 } as const
 
 /**
@@ -70,59 +72,60 @@ export type RouteConfig = {
   /** 메인 홈 화면 */
   [RoutePath.root]: { pathname: typeof RoutePath.root }
 
-  /** 계정 페이지 */
-  [RoutePath.account]: { pathname: typeof RoutePath.account }
-
   /** 로그인 페이지 */
   [RoutePath.login]: { pathname: typeof RoutePath.login }
-
-  /** 게스트 로그인 페이지 */
-  [RoutePath.guestLogin]: { pathname: typeof RoutePath.guestLogin }
-
-  /** 이메일 로그인 페이지 */
-  [RoutePath.emailLogin]: { pathname: typeof RoutePath.emailLogin }
-
-  /** 회원가입 페이지 */
-  [RoutePath.signUp]: { pathname: typeof RoutePath.signUp }
-
-  /** 랜덤 퀴즈 페이지 - 검색 파라미터로 카테고리와 레벨 지정 가능 */
+  
+  /** 설치 가이드 페이지 */
+  [RoutePath.installGuide]: { pathname: typeof RoutePath.installGuide }
+  
+  /** 노트 관련 */
+  [RoutePath.notes]: { pathname: typeof RoutePath.notes }
+  [RoutePath.noteDetail]: { pathname: typeof RoutePath.noteDetail }
+  [RoutePath.noteQuiz]: { pathname: typeof RoutePath.noteQuiz }
+  [RoutePath.noteArrange]: { pathname: typeof RoutePath.noteArrange }
+  [RoutePath.noteSearch]: { pathname: typeof RoutePath.noteSearch }
+  [RoutePath.noteEdit]: { pathname: typeof RoutePath.noteEdit }
+  [RoutePath.noteWrite]: { pathname: typeof RoutePath.noteWrite }
+  [RoutePath.noteUpload]: { pathname: typeof RoutePath.noteUpload }
+  
+  /** 계정 관련 */
+  [RoutePath.account]: { pathname: typeof RoutePath.account }
+  [RoutePath.accountInfo]: { pathname: typeof RoutePath.accountInfo }
+  [RoutePath.dailyQuizAttendance]: { pathname: typeof RoutePath.dailyQuizAttendance }
+  [RoutePath.quizAnalysis]: { pathname: typeof RoutePath.quizAnalysis }
+  [RoutePath.quizRecord]: { pathname: typeof RoutePath.quizRecord }
+  [RoutePath.notificationConfig]: { pathname: typeof RoutePath.notificationConfig }
+  [RoutePath.paymentHistory]: { pathname: typeof RoutePath.paymentHistory }
+  [RoutePath.notice]: { pathname: typeof RoutePath.notice }
+  [RoutePath.contact]: { pathname: typeof RoutePath.contact }
+  [RoutePath.faq]: { pathname: typeof RoutePath.faq }
+  [RoutePath.policy]: { pathname: typeof RoutePath.policy }
+  [RoutePath.withdraw]: { pathname: typeof RoutePath.withdraw }
+  
+  /** 테마 퀴즈 */
   [RoutePath.randomQuiz]: {
     pathname: typeof RoutePath.randomQuiz
     search: {
-      /** 퀴즈 카테고리 */
-      category: ReadonlyArray<string>
-      /** 난이도 레벨 */
-      level: ReadonlyArray<string>
+      /** 날짜 */
+      date: string
     }
   }
-
-  /** 밤 퀴즈 페이지 - 검색 파라미터로 카테고리와 레벨 지정 가능 */
   [RoutePath.bombQuiz]: {
     pathname: typeof RoutePath.bombQuiz
     search: {
-      /** 퀴즈 카테고리 */
-      category: ReadonlyArray<string>
-      /** 난이도 레벨 */
-      level: ReadonlyArray<string>
+      /** 날짜 */
+      date: string
     }
   }
-
-  /** 메인 퀴즈 페이지 */
-  [RoutePath.quiz]: { pathname: typeof RoutePath.quiz }
-
-  /** 노트 리스트 페이지 */
-  [RoutePath.notes]: { pathname: typeof RoutePath.notes }
-
-  /** 노트 상세 페이지 (동적 파라미터: noteId) */
-  [RoutePath.note]: { pathname: typeof RoutePath.note }
-
-  /** 컬렉션 리스트 페이지 */
+  
+  /** 컬렉션 관련 */
   [RoutePath.collections]: { pathname: typeof RoutePath.collections }
-
-  /** 컬렉션 상세 페이지 (동적 파라미터: collectionId) */
-  [RoutePath.collection]: { pathname: typeof RoutePath.collection }
-
-  /** 컬렉션 검색 페이지 - 검색 파라미터로 검색어 지정 가능 */
+  [RoutePath.collectionDetail]: { pathname: typeof RoutePath.collectionDetail }
+  [RoutePath.collectionQuiz]: { pathname: typeof RoutePath.collectionQuiz }
+  [RoutePath.collectionComplain]: { pathname: typeof RoutePath.collectionComplain }
+  [RoutePath.collectionCreate]: { pathname: typeof RoutePath.collectionCreate }
+  [RoutePath.collectionEditInfo]: { pathname: typeof RoutePath.collectionEditInfo }
+  [RoutePath.collectionEditQuiz]: { pathname: typeof RoutePath.collectionEditQuiz }
   [RoutePath.collectionSearch]: {
     pathname: typeof RoutePath.collectionSearch
     search: {
@@ -130,7 +133,4 @@ export type RouteConfig = {
       query: string
     }
   }
-
-  /** 설정 페이지 */
-  [RoutePath.setting]: { pathname: typeof RoutePath.setting }
 }
