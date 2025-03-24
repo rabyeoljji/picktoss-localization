2b88868fdc17f955b81f7a45266d83ec88d6c245
nageuna922@gmail.com <nageuna922@gmail.com>
2025-03-24 16:39:55 +0900

    feat: orderbox

---
+++ src/shared/components/ui/orderbox/index.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+
+import { Text } from '@/shared/components/ui/text'
+import { cn } from '@/shared/lib/utils'
+
+interface Props extends React.ComponentProps<typeof CheckboxPrimitive.Root> {
+  order?: number
+}
+
+function Orderbox({ className, order = 1, ...props }: Props) {
+  return (
+    <CheckboxPrimitive.Root
+      data-slot="checkbox"
+      className={cn(
+        'peer size-[24px] shrink-0 cursor-pointer rounded-full border border-gray-200 bg-white outline-none disabled:cursor-default disabled:border-gray-100 disabled:bg-gray-50 data-[state=checked]:border-orange-500 data-[state=checked]:bg-orange-500',
+        className,
+      )}
+      {...props}
+    >
+      <CheckboxPrimitive.Indicator data-slot="checkbox-indicator" className="flex-center transition-none p-[1px]">
+        {order != null && order > 0 && (
+          <Text typo="body-2-bold" color="inverse">
+            {order}
+          </Text>
+        )}
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  )
+}
+
+export { Orderbox }
