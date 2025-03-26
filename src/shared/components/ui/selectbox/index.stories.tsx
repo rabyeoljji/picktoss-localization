e4477d93bd624419ddbc59b17022e8f5bb1430b2
nageuna922@gmail.com <nageuna922@gmail.com>
2025-03-26 15:33:45 +0900

    feat: select box

--- src/shared/components/ui/selectbox/index.stories.tsx
+++ src/shared/components/ui/selectbox/index.stories.tsx
@@ -5,24 +5,31 @@ import { Selectbox } from '@/shared/components/ui/selectbox'
 const meta: Meta<typeof Selectbox> = {
   title: 'UI/Selectbox',
   component: Selectbox,
+  decorators: [
+    (Story) => (
+      <div className="w-full flex-center">
+        <Story />
+      </div>
+    ),
+  ],
 }
 export default meta
 
 export const AllCases: StoryObj<typeof Selectbox> = {
   render: () => {
     return (
-      <div className="flex items-center gap-10">
-        <div className="flex flex-col items-center gap-2.5">
+      <div className="w-xl flex flex-col items-center gap-10">
+        <div className="w-full flex flex-col items-center gap-2.5">
           <span>Default</span>
-          <Selectbox />
+          <Selectbox>생성한 결과물이 만족스럽지 않아요</Selectbox>
         </div>
-        <div className="flex flex-col items-center gap-2.5">
+        <div className="w-full flex flex-col items-center gap-2.5">
           <span>Checked</span>
-          <Selectbox defaultChecked />
+          <Selectbox defaultChecked>생성한 결과물이 만족스럽지 않아요</Selectbox>
         </div>
-        <div className="flex flex-col items-center gap-2.5">
+        <div className="w-full flex flex-col items-center gap-2.5">
           <span>Disabled</span>
-          <Selectbox disabled />
+          <Selectbox disabled>생성한 결과물이 만족스럽지 않아요</Selectbox>
         </div>
       </div>
     )
--- src/shared/components/ui/selectbox/index.tsx
+++ src/shared/components/ui/selectbox/index.tsx
@@ -9,7 +9,7 @@ function Selectbox({ className, children, ...props }: React.ComponentProps<typeo
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        'peer flex-center px-[20px] pt-[20px] pb-[12px] shrink-0 m-0 cursor-pointer rounded-[20px] border border-outline bg-white text-secondary outline-none disabled:cursor-default disabled:border-gray-100 disabled:bg-gray-50 disabled:text-disabled data-[state=checked]:border-accent data-[state=checked]:bg-accent data-[state=checked]:text-accent',
+        'peer flex-center w-full px-[16px] py-[14px] m-0 cursor-pointer rounded-[10px] border border-outline bg-base-1 text-secondary typo-body-1-medium outline-none disabled:cursor-default disabled:border-gray-100 disabled:bg-gray-50 disabled:text-disabled data-[state=checked]:border-accent data-[state=checked]:bg-accent data-[state=checked]:text-accent',
         className,
       )}
       {...props}
