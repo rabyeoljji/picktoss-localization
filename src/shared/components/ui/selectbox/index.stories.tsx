47cb7c4e49f49740466804a755dd4fde2ae46b57
nageuna922@gmail.com <nageuna922@gmail.com>
2025-03-26 18:01:32 +0900

    feat: select box

--- src/shared/components/ui/selectbox/index.stories.tsx
+++ src/shared/components/ui/selectbox/index.stories.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useState } from '@storybook/preview-api'
 import { Meta, StoryObj } from '@storybook/react'
 
+import { RadioGroup, RadioGroupItem } from '@/shared/components/ui/radio-group'
 import { Selectbox } from '@/shared/components/ui/selectbox'
 
 const meta: Meta<typeof Selectbox> = {
@@ -15,23 +18,52 @@ const meta: Meta<typeof Selectbox> = {
 }
 export default meta
 
+/**
+ * 선택 가능한 UI 컴포넌트로, 라디오 버튼이나 체크박스와 연계하여 사용 가능
+ */
 export const AllCases: StoryObj<typeof Selectbox> = {
   render: () => {
+    const [selectedValue, setSelectedValue] = useState('1')
+
     return (
-      <div className="w-xl flex flex-col items-center gap-10">
-        <div className="w-full flex flex-col items-center gap-2.5">
-          <span>Default</span>
-          <Selectbox>생성한 결과물이 만족스럽지 않아요</Selectbox>
+      <RadioGroup value={selectedValue} onValueChange={setSelectedValue}>
+        <div className="w-xl flex flex-col items-center gap-10">
+          <div className="w-full flex flex-col items-center gap-2.5">
+            <span>Default</span>
+            <Selectbox
+              relativeItem={<RadioGroupItem value={'0'} id={`type-0`} className="peer sr-only" />}
+              htmlFor={`type-0`}
+              selected={selectedValue === '0'}
+              onSelect={() => setSelectedValue('0')}
+            >
+              생성한 결과물이 만족스럽지 않아요
+            </Selectbox>
+          </div>
+          <div className="w-full flex flex-col items-center gap-2.5">
+            <span>Selected</span>
+            <Selectbox
+              relativeItem={<RadioGroupItem value={'1'} id={`type-1`} className="peer sr-only" />}
+              htmlFor={`type-1`}
+              selected={selectedValue === '1'}
+              onSelect={() => setSelectedValue('1')}
+            >
+              생성한 결과물이 만족스럽지 않아요
+            </Selectbox>
+          </div>
+          <div className="w-full flex flex-col items-center gap-2.5">
+            <span>Disabled</span>
+            <Selectbox
+              relativeItem={<RadioGroupItem value={'2'} id={`type-2`} className="peer sr-only" />}
+              htmlFor={`type-2`}
+              selected={selectedValue === '2'}
+              onSelect={() => setSelectedValue('2')}
+              disabled
+            >
+              생성한 결과물이 만족스럽지 않아요
+            </Selectbox>
+          </div>
         </div>
-        <div className="w-full flex flex-col items-center gap-2.5">
-          <span>Checked</span>
-          <Selectbox defaultChecked>생성한 결과물이 만족스럽지 않아요</Selectbox>
-        </div>
-        <div className="w-full flex flex-col items-center gap-2.5">
-          <span>Disabled</span>
-          <Selectbox disabled>생성한 결과물이 만족스럽지 않아요</Selectbox>
-        </div>
-      </div>
+      </RadioGroup>
     )
   },
 }
--- src/shared/components/ui/selectbox/index.tsx
+++ src/shared/components/ui/selectbox/index.tsx
@@ -1,22 +1,74 @@
 import * as React from 'react'
 
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
-
 import { cn } from '@/shared/lib/utils'
 
-function Selectbox({ className, children, ...props }: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
-  return (
-    <CheckboxPrimitive.Root
-      data-slot="checkbox"
-      className={cn(
-        'peer flex-center w-full px-[16px] py-[14px] m-0 cursor-pointer rounded-[10px] border border-outline bg-base-1 text-secondary typo-body-1-medium outline-none disabled:cursor-default disabled:border-gray-100 disabled:bg-gray-50 disabled:text-disabled data-[state=checked]:border-accent data-[state=checked]:bg-accent data-[state=checked]:text-accent',
-        className,
-      )}
-      {...props}
-    >
-      {children}
-    </CheckboxPrimitive.Root>
-  )
+interface SelectboxProps extends React.HTMLAttributes<HTMLLabelElement> {
+  selected: boolean
+  onSelect?: () => void
+
+  relativeItem?: React.ReactNode
+  htmlFor?: string
+  disabled?: boolean
 }
 
-export { Selectbox }
+/**
+ * 선택 가능한 UI 컴포넌트로, 라디오 버튼이나 체크박스와 연계하여 사용 가능
+ *
+ * @param selected 현재 선택 상태
+ * @param onSelect 선택 시 호출되는 콜백 함수
+ * @param relativeItem SelectBox와 연계할 라디오 버튼이나 체크박스 등의 아이템
+ * @param htmlFor 연결된 요소의 ID
+ * @param disabled 컴포넌트의 비활성화 상태
+ *
+ * @example
+ * // 단일 선택 (라디오 버튼)
+ * <Selectbox
+ *   selected={selectedValue === 'option1'}
+ *   onSelect={() => handleSelect('option1')}
+ *   relativeItem={<RadioButton />}
+ * >
+ *   Option 1
+ * </Selectbox>
+ *
+ * @example
+ * // 다중 선택 (체크박스)
+ * <Selectbox
+ *   selected={isChecked}
+ *   onSelect={handleToggle}
+ *   relativeItem={<Checkbox />}
+ * >
+ *   Multiple Select Option
+ * </Selectbox>
+ */
+export function Selectbox({
+  className,
+  children,
+  selected,
+  onSelect,
+  relativeItem,
+  htmlFor,
+  disabled,
+  ...props
+}: SelectboxProps) {
+  return (
+    <>
+      {relativeItem}
+      <label
+        htmlFor={htmlFor}
+        aria-checked={selected}
+        onClick={onSelect}
+        className={cn(
+          'flex-center w-full px-[16px] py-[14px] m-0 cursor-pointer rounded-[10px] border outline-none',
+          disabled ? 'cursor-default border-gray-100 !bg-gray-50 !text-disabled' : '',
+          selected
+            ? 'border-accent bg-accent text-accent typo-body-1-bold'
+            : 'border-outline bg-base-1 text-secondary typo-body-1-medium',
+          className,
+        )}
+        {...props}
+      >
+        {children}
+      </label>
+    </>
+  )
+}
