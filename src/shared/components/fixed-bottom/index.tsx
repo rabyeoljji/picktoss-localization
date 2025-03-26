ddef7da2fcbc1de3eef62f7faa04479305aacf5b
nageuna922@gmail.com <nageuna922@gmail.com>
2025-03-26 14:24:59 +0900

    feat: on boarding

---
+++ src/shared/components/fixed-bottom/index.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/shared/lib/utils'
+
+const FixedBottom = ({ children, className }: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      className={cn(
+        'fixed bottom-0 right-1/2 translate-x-1/2 z-50 h-fit w-full max-w-xl max-w-mobile bg-white px-[16px] pt-[14px] pb-[24px] items-start',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default FixedBottom
