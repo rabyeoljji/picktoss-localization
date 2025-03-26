da947b975a7799cb9af9cc892ae7e9f04a4ae73e
nageuna922@gmail.com <nageuna922@gmail.com>
2025-03-26 16:48:43 +0900

    fix: network error

--- src/app/layout/root-layout.tsx
+++ src/app/layout/root-layout.tsx
@@ -1,13 +1,9 @@
 import { Outlet } from 'react-router'
 
-import ErrorWrapper from '@/app/error'
-
 export const RootLayout = () => {
   return (
     <div className="mx-auto h-screen max-w-xl bg-gray-50">
-      <ErrorWrapper>
-        <Outlet />
-      </ErrorWrapper>
+      <Outlet />
     </div>
   )
 }
