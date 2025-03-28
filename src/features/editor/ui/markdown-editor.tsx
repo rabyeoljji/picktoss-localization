624e4e1d9950920debef8a00a246441195ea14f4
nageuna922@gmail.com <nageuna922@gmail.com>
2025-03-28 17:31:21 +0900

    fix: editor

--- src/features/editor/ui/markdown-editor.tsx
+++ src/features/editor/ui/markdown-editor.tsx
@@ -5,6 +5,7 @@ import Link from '@tiptap/extension-link'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { marked } from 'marked'
+import { toast } from 'sonner'
 import styled from 'styled-components'
 
 import { DOCUMENT_CONSTRAINTS } from '@/features/note/model/schema'
@@ -49,51 +50,26 @@ export const MarkdownEditor = ({
     content: marked(initialMarkdown, { gfm: true, breaks: true }),
     onUpdate: ({ editor }) => {
       if (onChange) {
-        const markdown = editor.getText() // 현재 텍스트 가져오기
+        const markdown = editor.getText()
 
         if (markdown.length <= DOCUMENT_CONSTRAINTS.CONTENT.MAX) {
           onChange(markdown)
         } else {
-          // 텍스트 자르기 (maxLength 기준)
           const truncatedContent = markdown.slice(0, DOCUMENT_CONSTRAINTS.CONTENT.MAX)
+          const currentSelection = editor.state.selection
 
-          const editorState = editor.state
-          const currentSelection = editorState.selection
-
-          // 트랜잭션을 이용해 텍스트 잘라내기
-          const tr = editorState.tr
-            .setSelection(currentSelection) // 현재 선택 영역 유지
-            .replaceWith(
-              0, // 문서의 처음부터 끝까지 텍스트를 교체
-              editorState.doc.content.size,
-              editorState.schema.text(truncatedContent), // 잘라낸 텍스트로 교체
-            )
-
-          // 트랜잭션 적용
-          editor.view.dispatch(tr)
-
-          // 잘라낸 텍스트 상태 반영
+          editor.view.updateState(
+            editor.state.apply(
+              editor.state.tr
+                .setSelection(currentSelection)
+                .replaceWith(0, editor.state.doc.content.size, editor.state.schema.text(truncatedContent)),
+            ),
+          )
           onChange(truncatedContent)
-
-          // 텍스트 변경 후 커서 위치 다시 설정
-          editor.view.focus()
+          toast('내용은 50,000자까지 작성 가능합니다')
         }
       }
     },
-    // onUpdate: ({ editor }) => {
-    //   if (onChange) {
-    //     const markdown = editor.getText()
-    //     // For markdown, we'll just use the text content directly
-
-    //     if (markdown.length <= DOCUMENT_CONSTRAINTS.CONTENT.MAX) {
-    //       onChange(markdown)
-    //     } else {
-    //       const truncatedContent = markdown.slice(0, DOCUMENT_CONSTRAINTS.CONTENT.MAX)
-
-    //       onChange(truncatedContent)
-    //     }
-    //   }
-    // },
     onFocus: () => setIsFocused(true),
     onBlur: () => setIsFocused(false),
   })
