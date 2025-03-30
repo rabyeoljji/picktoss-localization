a023c5628225832fe2d26af2c27fb8e4c0f37c30
nageuna922@gmail.com <nageuna922@gmail.com>
2025-03-30 16:37:38 +0900

    style: upload file

--- src/features/note/ui/note-create-page-file/index.tsx
+++ src/features/note/ui/note-create-page-file/index.tsx
@@ -3,18 +3,24 @@ import { useEffect } from 'react'
 import { KeyboardDetector } from '@/app/keyboard-detector'
 
 import { MarkdownEditor } from '@/features/editor'
+import { formatFileSize } from '@/features/note/lib'
 import { useCreateNoteContext } from '@/features/note/model/create-note-context'
 
-import { IcChange } from '@/shared/assets/icon'
+import { IcChange, IcFile } from '@/shared/assets/icon'
 import { Text } from '@/shared/components/ui/text'
 
 const NoteCreatePageFile = () => {
   const { setDocumentName, content, setContent, setIsKeyboardVisible, fileInfo, changeFileInfo, isProcessing } =
     useCreateNoteContext()
 
+  const removeFileExtension = (filename: string) => {
+    const lastDotIndex = filename.lastIndexOf('.')
+    return lastDotIndex > 0 ? filename.slice(0, lastDotIndex) : filename
+  }
+
   useEffect(() => {
     if (fileInfo) {
-      setDocumentName(fileInfo.name)
+      setDocumentName(removeFileExtension(fileInfo.name))
       setContent({ markdown: fileInfo.content, textLength: fileInfo.charCount })
     } else {
       setDocumentName('')
@@ -56,7 +62,20 @@ const NoteCreatePageFile = () => {
         </div>
       )}
 
-      <div className="fixed z-40 bottom-[64px] left-1/2 -translate-x-1/2 size-fit">
+      <div className="fixed z-40 bottom-[64px] flex-center shadow-[var(--shadow-md)] left-1/2 -translate-x-1/2 w-[90dvw] max-w-[430px] py-[8px] px-[12px] rounded-[16px] border border-outline bg-base-1 gap-[16px]">
+        <div className="flex-center rounded-[12px] bg-inverse text-icon-inverse p-[16px]">
+          <IcFile className="size-[24px]" />
+        </div>
+
+        <div className="flex flex-col w-[calc(100%-168px)]">
+          <Text typo="subtitle-2-bold" color="secondary" className="w-full truncate">
+            {fileInfo?.name}
+          </Text>
+          <Text typo="body-1-medium" color="sub">
+            {formatFileSize(fileInfo?.size ?? 0)}
+          </Text>
+        </div>
+
         <input
           type="file"
           name="uploadFileInput"
@@ -67,11 +86,11 @@ const NoteCreatePageFile = () => {
         />
         <label
           htmlFor="uploadFileInput"
-          className="cursor-pointer block shadow-[var(--shadow-md)] relative inline-flex items-center rounded-full cursor-pointer justify-center border border-gray-100 bg-white text-gray-700 hover:bg-gray-50 active:bg-gray-100 typo-button-3 h-[44px] w-fit px-4 py-3.5 disabled:pointer-events-none shrink-0 outline-none disabled:bg-gray-100 disabled:text-gray-200"
+          className="cursor-pointer block relative inline-flex items-center rounded-full cursor-pointer justify-center border border-gray-100 bg-white text-gray-700 hover:bg-gray-50 active:bg-gray-100 typo-button-3 h-[32px] min-w-[60px] px-2.5 py-2 w-fit disabled:pointer-events-none shrink-0 outline-none disabled:bg-gray-100 disabled:text-gray-200"
         >
           <IcChange className="size-[20px]" />
           <Text typo="button-3" className="px-1">
-            파일 변경
+            변경
           </Text>
         </label>
       </div>
