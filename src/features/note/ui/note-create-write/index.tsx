15d06b3310ba3185a89ff4be4da27ee164c7769a
jw-r <evencoding@gmail.com>
2025-04-06 14:32:56 +0900

    chore: test editor

--- src/features/note/ui/note-create-write/index.tsx
+++ src/features/note/ui/note-create-write/index.tsx
@@ -22,11 +22,33 @@ export const NoteCreateWrite = () => {
   }
 
   useEffect(() => {
-    if (!textareaWrapperRef.current) return
+    const updateHeight = () => {
+      // visualViewport가 존재하면 현재 클라이언트 높이(키보드 반영된)를 사용
+      const newHeight = window.visualViewport
+        ? window.visualViewport.height
+        : textareaWrapperRef.current?.offsetHeight || 300
+      setTextareaHeight(newHeight)
+      // 항상 스크롤을 최상위로
+      window.scrollTo(0, 0)
+    }
 
-    const textareaWrapperHeight = textareaWrapperRef.current?.offsetHeight || 300
-    setTextareaHeight(textareaWrapperHeight)
-  }, [isKeyboardVisible])
+    updateHeight()
+
+    // visualViewport가 있으면 해당 이벤트에, 없으면 window resize 이벤트에 등록
+    if (window.visualViewport) {
+      window.visualViewport.addEventListener('resize', updateHeight)
+    } else {
+      window.addEventListener('resize', updateHeight)
+    }
+
+    return () => {
+      if (window.visualViewport) {
+        window.visualViewport.removeEventListener('resize', updateHeight)
+      } else {
+        window.removeEventListener('resize', updateHeight)
+      }
+    }
+  }, [])
 
   return (
     <>
@@ -36,7 +58,8 @@ export const NoteCreateWrite = () => {
           className="border-none px-4"
           onChange={(e) => handleTextareaChange(e.target.value)}
           style={{
-            height: textareaHeight - (isKeyboardVisible ? 40 : 96),
+            // visualViewport에 따른 높이 사용 (추가 오프셋이 필요하면 여기서 조절 가능)
+            height: textareaHeight,
           }}
         />
       </div>
