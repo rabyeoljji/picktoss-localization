8820291fc970d52fb4d882646c4caf1b1c675cfe
jw-r <evencoding@gmail.com>
2025-04-16 21:17:32 +0900

    refactor: create quiz

--- src/features/note/ui/create-note-drawer/index.tsx
+++ src/features/note/ui/create-note-drawer/index.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import { AnimatePresence, motion } from 'framer-motion'
 
-import { useCreateNoteContext } from '@/features/note/model/create-note-context'
+import { QuizType, useCreateNoteContext } from '@/features/note/model/create-note-context'
 
 import { useGetCategories } from '@/entities/category/api/hooks'
 
@@ -56,97 +56,66 @@ export const CreateNoteDrawer = () => {
       <DrawerContent height="md">
         <div className={cn('overflow-y-scroll p-4', checkCreateActivate() && 'pb-[114px]')}>
           <div className="grid gap-2">
+            {/* 카테고리 선택 섹션 */}
             <div className="border rounded-[12px] bg-surface-1 border-outline">
-              {/* Trigger */}
-              <button
-                onClick={toggleCategoryExpand}
-                className="w-full py-[14px] flex justify-between px-5 items-center typo-subtitle-2-bold focus:outline-none"
-              >
-                <span>
-                  {selectedCategory && !isCategoryExpanded
-                    ? `${selectedCategory?.emoji}\u00A0\u00A0${selectedCategory?.name}`
-                    : '카테고리'}
-                </span>
-                {isCategoryExpanded ? (
-                  <IcChevronUp className="size-4 text-icon-secondary" />
-                ) : (
-                  <IcChevronDown className="size-4 text-icon-secondary" />
-                )}
-              </button>
-              <AnimatePresence>
-                {isCategoryExpanded && (
-                  <motion.div
-                    initial={{ opacity: 0, height: 0 }}
-                    animate={{ opacity: 1, height: 'auto' }}
-                    exit={{ opacity: 0, height: 0 }}
-                    transition={{ duration: 0.3 }}
-                  >
-                    {/* 카테고리 버튼 리스트 */}
-                    <div className="flex flex-col">
-                      {categories?.map((category) => (
-                        <button
-                          key={category.id}
-                          onClick={() => {
-                            setCategoryId(category.id)
-                            setIsCategoryExpanded(false)
-                          }}
-                          className="w-full px-4 py-[14px] hover:bg-gray-100 focus:outline-none text-left flex items-center justify-between"
-                        >
-                          <span>{`${category.emoji}\u00A0\u00A0${category.name}`}</span>
-                          {category.id === categoryId && <IcCheck className="size-6 text-icon-accent" />}
-                        </button>
-                      ))}
-                    </div>
-                  </motion.div>
-                )}
-              </AnimatePresence>
+              <ExpandableHeader
+                title={
+                  selectedCategory && !isCategoryExpanded
+                    ? `${selectedCategory.emoji}\u00A0\u00A0${selectedCategory.name}`
+                    : '카테고리'
+                }
+                isExpanded={isCategoryExpanded}
+                toggle={toggleCategoryExpand}
+              />
+              <CollapsibleSection isOpen={isCategoryExpanded}>
+                <div className="flex flex-col">
+                  {categories?.map((category) => (
+                    <button
+                      key={category.id}
+                      onClick={() => {
+                        setCategoryId(category.id)
+                        setIsCategoryExpanded(false)
+                      }}
+                      className="w-full px-4 py-[14px] hover:bg-gray-100 focus:outline-none text-left flex items-center justify-between"
+                    >
+                      <span>{`${category.emoji}\u00A0\u00A0${category.name}`}</span>
+                      {category.id === categoryId && <IcCheck className="size-6 text-icon-accent" />}
+                    </button>
+                  ))}
+                </div>
+              </CollapsibleSection>
             </div>
 
+            {/* 문제 유형 선택 섹션 */}
             <div className="border rounded-[12px] bg-surface-1 border-outline">
-              <button
-                onClick={toggleQuizTypeExpand}
-                className="w-full py-[14px] flex justify-between px-5 items-center typo-subtitle-2-bold focus:outline-none"
-              >
-                <span>
-                  {quizType && !isQuizTypeExpanded ? `${quizType === 'MIX_UP' ? 'O/X' : '객관식'}` : '문제 유형'}
-                </span>
-                {isQuizTypeExpanded ? (
-                  <IcChevronUp className="size-4 text-icon-secondary" />
-                ) : (
-                  <IcChevronDown className="size-4 text-icon-secondary" />
-                )}
-              </button>
-              <AnimatePresence>
-                {isQuizTypeExpanded && (
-                  <motion.div
-                    initial={{ opacity: 0, height: 0 }}
-                    animate={{ opacity: 1, height: 'auto' }}
-                    exit={{ opacity: 0, height: 0 }}
-                    transition={{ duration: 0.3 }}
-                  >
-                    <div className="flex flex-col">
-                      {[
-                        { value: 'MIX_UP', label: 'O/X' },
-                        { value: 'MULTIPLE_CHOICE', label: '객관식' },
-                      ]?.map((qt) => (
-                        <button
-                          key={qt.value}
-                          onClick={() => {
-                            setQuizType(qt.value as 'MIX_UP' | 'MULTIPLE_CHOICE')
-                            setIsQuizTypeExpanded(false)
-                          }}
-                          className="w-full px-4 py-[14px] hover:bg-gray-100 focus:outline-none text-left flex items-center justify-between"
-                        >
-                          <span>{qt.label}</span>
-                          {qt.value === quizType && <IcCheck className="size-6 text-icon-accent" />}
-                        </button>
-                      ))}
-                    </div>
-                  </motion.div>
-                )}
-              </AnimatePresence>
+              <ExpandableHeader
+                title={quizType && !isQuizTypeExpanded ? (quizType === 'MIX_UP' ? 'O/X' : '객관식') : '문제 유형'}
+                isExpanded={isQuizTypeExpanded}
+                toggle={toggleQuizTypeExpand}
+              />
+              <CollapsibleSection isOpen={isQuizTypeExpanded}>
+                <div className="flex flex-col">
+                  {[
+                    { value: 'MIX_UP', label: 'O/X' },
+                    { value: 'MULTIPLE_CHOICE', label: '객관식' },
+                  ].map((qt) => (
+                    <button
+                      key={qt.value}
+                      onClick={() => {
+                        setQuizType(qt.value as QuizType)
+                        setIsQuizTypeExpanded(false)
+                      }}
+                      className="w-full px-4 py-[14px] hover:bg-gray-100 focus:outline-none text-left flex items-center justify-between"
+                    >
+                      <span>{qt.label}</span>
+                      {qt.value === quizType && <IcCheck className="size-6 text-icon-accent" />}
+                    </button>
+                  ))}
+                </div>
+              </CollapsibleSection>
             </div>
 
+            {/* 퀴즈 공개 여부 스위치 */}
             <div className="flex items-center justify-between px-2 py-[12px]">
               <div className="flex gap-2 items-end">
                 <Text typo="body-1-bold" color="secondary">
@@ -181,3 +150,40 @@ export const CreateNoteDrawer = () => {
     </Drawer>
   )
 }
+
+const ExpandableHeader = ({
+  title,
+  isExpanded,
+  toggle,
+}: {
+  title: string
+  isExpanded: boolean
+  toggle: () => void
+}) => (
+  <button
+    onClick={toggle}
+    className="w-full py-[14px] flex justify-between px-5 items-center typo-subtitle-2-bold focus:outline-none"
+  >
+    <span>{title}</span>
+    {isExpanded ? (
+      <IcChevronUp className="size-4 text-icon-secondary" />
+    ) : (
+      <IcChevronDown className="size-4 text-icon-secondary" />
+    )}
+  </button>
+)
+
+const CollapsibleSection = ({ isOpen, children }: { isOpen: boolean; children: React.ReactNode }) => (
+  <AnimatePresence>
+    {isOpen && (
+      <motion.div
+        initial={{ opacity: 0, height: 0 }}
+        animate={{ opacity: 1, height: 'auto' }}
+        exit={{ opacity: 0, height: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        {children}
+      </motion.div>
+    )}
+  </AnimatePresence>
+)
