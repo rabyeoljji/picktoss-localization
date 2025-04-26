82edcfcb63743e5a329ef66c9fa1f45d2b6c594f
nageuna922@gmail.com <nageuna922@gmail.com>
2025-04-26 23:48:41 +0900

    feat: explore swiper

--- src/pages/explore/explore-page.tsx
+++ src/pages/explore/explore-page.tsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Marquee from 'react-fast-marquee'
+
+import { motion } from 'framer-motion'
+import SwiperCore from 'swiper'
+import { Mousewheel } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 import { withHOC } from '@/app/hoc/with-page-config'
 import HeaderOffsetLayout from '@/app/layout/header-offset-layout'
 
-import { IcLibrary, IcLogo, IcProfile, IcSearch } from '@/shared/assets/icon'
-import { BookmarkHorizontalCard } from '@/shared/components/cards/bookmark-horizontal-card'
+import { IcChevronRight, IcLibrary, IcLogo, IcProfile, IcSearch } from '@/shared/assets/icon'
 import { Header } from '@/shared/components/header'
-import { Button } from '@/shared/components/ui/button'
 import { Chip } from '@/shared/components/ui/chip'
 import HorizontalScrollContainer from '@/shared/components/ui/horizontal-scroll-container'
 import { Text } from '@/shared/components/ui/text'
@@ -58,43 +61,16 @@ const ExplorePage = () => {
   const [params, setParams] = useQueryParam('/explore')
   const activeTab = params.tab
 
-  const [isHeaderHidden, setIsHeaderHidden] = useState(false)
-
   type Tab = typeof params.tab
 
   const setTab = (tab: Tab) => {
     setParams({ ...params, tab })
   }
 
-  useEffect(() => {
-    let lastScrollY = window.scrollY
-
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY
-
-      if (currentScrollY > lastScrollY) {
-        // 스크롤을 내리면 헤더를 숨김
-        setIsHeaderHidden(true)
-      } else {
-        // 스크롤을 올리면 헤더를 보여줌
-        setIsHeaderHidden(false)
-      }
-
-      lastScrollY = currentScrollY
-    }
-
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
-
   return (
     <>
       <Header
-        className={cn(
-          'transition-all duration-300 ease-in-out',
-          isHeaderHidden ? 'opacity-0 pointer-events-none' : 'opacity-100',
-          'bg-surface-2 py-[9px] px-[8px]',
-        )}
+        className={cn('transition-all duration-300 ease-in-out', 'bg-surface-2 py-[9px] px-[8px]')}
         left={
           <button className="size-[40px] flex-center">
             <IcProfile className="size-[24px] text-icon-secondary" />
@@ -113,7 +89,34 @@ const ExplorePage = () => {
       />
 
       <HeaderOffsetLayout>
-        <div className="py-[42px] flex flex-col gap-[10px]">
+        {/* banner */}
+        <div className="py-[12px] px-[16px] w-full h-fit">
+          <button
+            type="button"
+            className="self-stretch h-14 w-full min-w-28 px-4 py-3 bg-accent rounded-[12px] inline-flex justify-center items-center gap-28"
+          >
+            <div className="flex-1 flex items-center">
+              <div className="flex items-center gap-2">
+                <IcLibrary className="size-[20px] text-icon-accent" />
+                <Text typo="body-1-bold" color="secondary">
+                  공개할 수 있는 퀴즈가{' '}
+                  <Text as="span" typo="body-1-bold" color="accent">
+                    3개
+                  </Text>{' '}
+                  있어요
+                </Text>
+              </div>
+            </div>
+            <IcChevronRight className="size-[16px] text-icon-secondary" />
+          </button>
+        </div>
+
+        <div
+          className="py-[42px] flex flex-col gap-[10px] bg-[radial-gradient(closest-side,_var(--tw-gradient-stops))]"
+          style={{
+            backgroundImage: 'radial-gradient(circle, var(--color-gray-100) 0%, var(--color-gray-50) 40%)',
+          }}
+        >
           <Marquee gradient={false} speed={20} direction="left">
             {exampleQuestions.map((item, index) => (
               <QuestionBox key={index} emoji={item.emoji} question={item.question} className="mr-[8px]" />
@@ -131,18 +134,14 @@ const ExplorePage = () => {
           </Marquee>
         </div>
 
-        <div className="pt-[22px]">
-          <Text as="h2" typo="h4" className="px-[16px] mb-[12px]">
-            실시간 퀴즈
+        <div className="pt-[25px]">
+          <Text as="h2" typo="h3" className="px-[16px] mb-[12px]">
+            오늘의 퀴즈
           </Text>
 
-          {isHeaderHidden && (
-            <div className="fixed top-0 z-50 bg-surface-2 h-[env(safe-area-inset-top)] w-full p-2"></div>
-          )}
-          <div
+          <motion.div
             className={cn(
-              'w-full py-[8px] sticky z-50 bg-[linear-gradient(to_bottom,#F8F8F7_25%,rgba(245,245,245,0)_100%)]',
-              isHeaderHidden ? 'top-[env(safe-area-inset-top)]' : 'top-[var(--header-height-safe)]',
+              'w-full py-[8px] sticky z-50 bg-[linear-gradient(to_bottom,#F8F8F7_28%,rgba(245,245,245,0)_100%)] top-[var(--header-height-safe)]',
             )}
           >
             <HorizontalScrollContainer
@@ -160,51 +159,10 @@ const ExplorePage = () => {
                 </Chip>
               ))}
             />
-          </div>
+          </motion.div>
 
-          <div className="w-full p-[16px] flex flex-col gap-[10px]">
-            {/* banner */}
-            <div className="self-stretch h-14 w-full min-w-28 px-4 py-3 bg-base-1 rounded-[12px] inline-flex justify-center items-center gap-28">
-              <div className="flex-1 flex items-center">
-                <div className="flex items-center gap-2">
-                  <IcLibrary className="size-[20px] text-icon-accent" />
-                  <Text typo="body-1-bold" color="secondary">
-                    공개할 수 있는 퀴즈가{' '}
-                    <Text as="span" typo="body-1-bold" color="accent">
-                      3개
-                    </Text>{' '}
-                    있어요
-                  </Text>
-                </div>
-              </div>
-              <Button variant={'secondary1'} size={'xs'}>
-                확인하기
-              </Button>
-            </div>
-
-            {/* content */}
-            {Array.from({ length: 10 }).map((_, index) => (
-              <Link key={index} to={'/note/:noteId'} params={[String(1)]}>
-                <BookmarkHorizontalCard>
-                  <BookmarkHorizontalCard.Left content="📄" />
-
-                  <BookmarkHorizontalCard.Content>
-                    <BookmarkHorizontalCard.Header
-                      title="금융투자분석사 노트정리"
-                      isBookmarked={true}
-                      onClickBookmark={() => alert('click bookmark')}
-                    />
-                    <BookmarkHorizontalCard.Preview content="1. 금융투자분석사 개요 금융투자분석사는 투자분석 및 포트폴리오 관리를 수행하는 전문가로서" />
-                    <BookmarkHorizontalCard.Detail
-                      quizCount={28}
-                      playedCount={345}
-                      bookmarkCount={21}
-                      isShared={true}
-                    />
-                  </BookmarkHorizontalCard.Content>
-                </BookmarkHorizontalCard>
-              </Link>
-            ))}
+          <div className="w-full h-[calc(100vh-184px)] p-[16px] pt-[36px] flex flex-col items-center gap-[10px] overflow-hidden">
+            <VerticalSwipeList />
           </div>
         </div>
       </HeaderOffsetLayout>
@@ -237,5 +195,70 @@ const QuestionBox = ({
         </Text>
       </div>
     </div>
+  )
+}
+
+function VerticalSwipeList() {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const swiperRef = useRef<SwiperCore>(null)
+
+  useEffect(() => {
+    const handleScroll = (event: WheelEvent) => {
+      const swiperTopOffset = swiperRef.current?.el?.getBoundingClientRect().top ?? 0
+      const scrollingUp = event.deltaY < 0
+
+      if (!swiperRef.current) return
+
+      if (swiperTopOffset <= 136) {
+        if (scrollingUp && activeIndex === 0) {
+          // 위로 스크롤 + 첫 번째 카드일 때 swiper 비활성화
+          swiperRef.current.mousewheel.disable()
+          swiperRef.current.allowTouchMove = false
+        } else {
+          // 그 외에는 swiper 활성화
+          swiperRef.current.mousewheel.enable()
+          swiperRef.current.allowTouchMove = true
+        }
+      } else {
+        // swiper가 화면 아래쪽이면 swiper 비활성화
+        swiperRef.current.mousewheel.disable()
+        swiperRef.current.allowTouchMove = false
+      }
+    }
+
+    window.addEventListener('wheel', handleScroll, { passive: false })
+
+    return () => {
+      window.removeEventListener('wheel', handleScroll)
+    }
+  }, [activeIndex])
+
+  return (
+    <Swiper
+      direction="vertical"
+      slidesPerView={1}
+      spaceBetween={0.01}
+      mousewheel={{
+        forceToAxis: true,
+        enabled: false, // 초기에 비활성화
+      }}
+      modules={[Mousewheel]}
+      onSwiper={(swiper) => (swiperRef.current = swiper)}
+      onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+      style={{ height: '508px', width: '100%', display: 'flex', justifyContent: 'center' }}
+    >
+      {Array.from({ length: 3 }).map((_, index) => (
+        <SwiperSlide key={index}>
+          <div
+            className={cn(
+              'w-[311px] h-[462px] relative bg-gradient-to-b from-bg-surface-1 to-bg-accent rounded-[20px] shadow-[0px_4px_28px_0px_rgba(0,0,0,0.10)] overflow-hidden transition-transform duration-300 swiper-slide-active:scale-110',
+              activeIndex === index && 'scale-110',
+            )}
+          >
+            card {index + 1}
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   )
 }
