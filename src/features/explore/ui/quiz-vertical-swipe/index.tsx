9054a71a88fa6bd138f20019a5456e992725e977
nageuna <nageuna922@gmail.com>
2025-05-02 17:26:21 +0900

    fix: explore swipe card pwa

--- src/features/explore/ui/quiz-vertical-swipe/index.tsx
+++ src/features/explore/ui/quiz-vertical-swipe/index.tsx
@@ -100,7 +100,7 @@ const QuizVerticalSwipe = () => {
   }
 
   return (
-    <div className="w-full h-[calc(100vh-_(env(--header-height-safe)_+_46px_+_84px))] p-[16px] pt-[48px] flex flex-col items-center gap-[10px] overflow-hidden">
+    <div className="sticky top-[calc(env(--header-height-safe)+46px)] w-full h-[calc(100dvh-184px)] p-[16px] pt-[48px] flex flex-col items-center gap-[10px] overflow-hidden">
       <Swiper
         direction="vertical"
         slidesPerView={1}
