25363f72117433e1201cd32ee066476103516ab1
nageuna <nageuna922@gmail.com>
2025-05-20 00:45:43 +0900

    feat: account main page publishing

--- src/pages/account/account-info-page.tsx
+++ src/pages/account/account-info-page.tsx
@@ -1,258 +1,254 @@
-// import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
-// import { toast } from 'sonner'
+import { toast } from 'sonner'
 
-// import HeaderOffsetLayout from '@/app/layout/header-offset-layout'
+import HeaderOffsetLayout from '@/app/layout/header-offset-layout'
 
-// import { useAuthStore } from '@/features/auth'
-// import InterestedCategoryDrawer from '@/features/user/ui/interested-category-drawer'
+import { useAuthStore } from '@/features/auth'
+import InterestedCategoryDrawer from '@/features/user/ui/interested-category-drawer'
 
-// import { useUpdateMemberName, useUser } from '@/entities/member/api/hooks'
+import { useUpdateMemberName, useUser } from '@/entities/member/api/hooks'
 
-// import { IcCamera, IcChevronRight, IcMy } from '@/shared/assets/icon'
-// import { ImgRoundGoogle, ImgRoundKakao } from '@/shared/assets/images'
-// import { BackButton } from '@/shared/components/buttons/back-button'
-// import { Header } from '@/shared/components/header'
-// import { SystemDialog } from '@/shared/components/system-dialog'
-// import { Input } from '@/shared/components/ui/input'
-// import { Text } from '@/shared/components/ui/text'
-// import { TextButton } from '@/shared/components/ui/text-button'
-// import { CategoryEnum } from '@/shared/config'
-// import { RoutePath, useRouter } from '@/shared/lib/router'
-
-// const AccountInfoPage = () => {
-//   const router = useRouter()
-
-//   const { clearToken } = useAuthStore()
-//   const { data: user } = useUser()
-
-//   const [nameDialogOpen, setNameDialogOpen] = useState<boolean>(false)
-//   const [logoutDialogOpen, setLogoutDialogOpen] = useState<boolean>(false)
-//   const [newName, setNewName] = useState<string>(user.name)
-//   const [imageUrl, setImageUrl] = useState<string | null>(null)
-
-//   const { mutate: updateMemberName, isPending } = useUpdateMemberName()
-
-//   const interestCategories = useMemo(
-//     () => (user.interestCategories?.length ? user.interestCategories : ['관심 분야 없음']),
-//     [user.interestCategories],
-//   )
-
-//   // user name이 변경될 경우 dialog input 기본 값도 변경
-//   useEffect(() => {
-//     setNewName(user.name)
-//   }, [user.name])
-
-//   // 로그아웃
-//   const handleLogout = () => {
-//     clearToken()
-//   }
-
-//   // 이름 변경 dialog open 핸들러
-//   const handleSetNameDialogOpen = (open: boolean) => {
-//     if (!open) {
-//       setNewName(user.name ?? '')
-//     }
-
-//     setNameDialogOpen(open)
-//   }
-
-//   // 이름 변경 핸들러
-//   const handleChangeName = () => {
-//     updateMemberName(
-//       { name: newName },
-//       {
-//         onSuccess: () => {
-//           toast('닉네임이 변경되었어요')
-//           setNameDialogOpen(false)
-//           setNewName(user.name ?? '')
-//         },
-//       },
-//     )
-//   }
-
-//   // File을 string으로 변환하는 함수
-//   const convertFileToString = (file: File): Promise<string> => {
-//     return new Promise((resolve, reject) => {
-//       const reader = new FileReader()
-//       reader.onload = () => resolve(reader.result as string)
-//       reader.onerror = reject
-//       reader.readAsDataURL(file)
-//     })
-//   }
-
-//   // 파일 선택 핸들러
-//   const handleChangeImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const file = e.target.files?.[0]
-//     if (file) {
-//       try {
-//         // File을 string(Base64)으로 변환
-//         const imageString = await convertFileToString(file)
-
-//         // TODO: 이미지 설정 논의
-//         // 여기서 서버에 이미지 업로드 로직을 구현 (서버에서 받는 형식에 따라 수정)
-//         // uploadImageToServer(file);
-
-//         // 성공 시
-//         setImageUrl(imageString)
-//         toast('프로필 사진이 변경되었어요')
-//       } catch (error) {
-//         console.error('이미지 변환 실패:', error)
-//         toast('이미지 처리 중 오류가 발생했습니다')
-//       }
-//     }
-//   }
-
-//   return (
-//     <div className="min-h-screen bg-base-1 flex flex-col">
-//       <Header left={<BackButton />} title="계정 정보" />
-
-//       <HeaderOffsetLayout className="w-full grow flex flex-col overflow-x-hidden px-[16px] justify-between">
-//         <div className="w-full flex flex-col">
-//           <div className="flex-center w-full pb-[44px] pt-[24px]">
-//             <div className="relative">
-//               <div className="flex-center relative size-[96px] overflow-hidden rounded-full bg-base-3">
-//                 {imageUrl ? (
-//                   <img src={imageUrl} alt="" className="object-cover" />
-//                 ) : (
-//                   <IcMy className="size-[56px] text-icon-sub" />
-//                 )}
-//               </div>
-//               {/* 이미지 등록 버튼 */}
-//               <input
-//                 type="file"
-//                 name="file"
-//                 id="userImage"
-//                 className="hidden"
-//                 accept="image/*"
-//                 onChange={handleChangeImage}
-//               />
-//               <label
-//                 htmlFor="userImage"
-//                 className="flex-center absolute z-10 bottom-0 right-0 size-[32px] cursor-pointer rounded-full border border-outline bg-base-1"
-//               >
-//                 <IcCamera className="size-[16px]" />
-//               </label>
-//             </div>
-//           </div>
-
-//           <div className="flex flex-col gap-[32px]">
-//             <button
-//               type="button"
-//               onClick={() => setNameDialogOpen(true)}
-//               className="flex w-full items-center justify-between"
-//             >
-//               <div className="flex flex-col items-start gap-[4px]">
-//                 <Text typo="body-1-medium" color="sub">
-//                   닉네임
-//                 </Text>
-
-//                 <Text typo="subtitle-2-medium">{user.name}</Text>
-//               </div>
-//               <IcChevronRight className="size-[16px] text-icon-sub" />
-//             </button>
-//             <SystemDialog
-//               open={nameDialogOpen}
-//               onOpenChange={handleSetNameDialogOpen}
-//               title="닉네임 변경"
-//               description="새로 사용할 이름을 입력해주세요"
-//               preventClose={isPending}
-//               content={
-//                 <>
-//                   {isPending ? (
-//                     <div className="animate-pulse text-center">Loading...</div>
-//                   ) : (
-//                     <Input
-//                       value={newName}
-//                       onChange={(e) => setNewName(e.target.value)}
-//                       placeholder={user.name}
-//                       max={10}
-//                       hasClear
-//                       onClearClick={() => setNewName('')}
-//                       helperText={`10자 이내로 입력해주세요 (${newName.length}/10)`}
-//                     />
-//                   )}
-//                 </>
-//               }
-//               onConfirm={handleChangeName}
-//               disabledConfirm={newName.length === 0}
-//             />
-
-//             <InterestedCategoryDrawer
-//               interestedCategories={interestCategories as (CategoryEnum | '관심 분야 없음')[]}
-//             />
-
-//             <div className="flex w-full items-center justify-between">
-//               <div className="flex flex-col items-start gap-[4px]">
-//                 <Text typo="body-1-medium" color="sub">
-//                   이메일
-//                 </Text>
-
-//                 <Text typo="subtitle-2-medium" color="primary">
-//                   {user.email ? user.email : '이메일 주소를 등록해주세요'}
-//                 </Text>
-//               </div>
-//             </div>
-
-//             <div className="flex w-full items-center justify-between">
-//               <div className="flex flex-col items-start gap-[4px]">
-//                 <Text typo="body-1-medium" color="sub">
-//                   로그인 정보
-//                 </Text>
-//                 <div className="flex items-center gap-[8px]">
-//                   {user.socialPlatform === 'KAKAO' ? (
-//                     <>
-//                       <ImgRoundKakao className="size-[20px]" />
-//                       <Text typo="subtitle-2-medium">카카오 로그인</Text>
-//                     </>
-//                   ) : (
-//                     <>
-//                       <ImgRoundGoogle className="size-[20px]" />
-//                       <Text typo="subtitle-2-medium">구글 로그인</Text>
-//                     </>
-//                   )}
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="flex-center pb-[56px]">
-//           <TextButton
-//             onClick={() => setLogoutDialogOpen(true)}
-//             variant={'sub'}
-//             size={'sm'}
-//             className="pr-[16px] border-r border-divider"
-//           >
-//             로그아웃
-//           </TextButton>
-//           <SystemDialog
-//             open={logoutDialogOpen}
-//             onOpenChange={setLogoutDialogOpen}
-//             title="로그아웃 하시겠어요?"
-//             cancelLabel="취소"
-//             confirmLabel="로그아웃"
-//             variant="critical"
-//             onConfirm={handleLogout}
-//           />
-//           <TextButton
-//             onClick={() => router.push(RoutePath.accountWithdraw)}
-//             variant={'sub'}
-//             size={'sm'}
-//             className="pl-[16px]"
-//           >
-//             탈퇴하기
-//           </TextButton>
-//         </div>
-//       </HeaderOffsetLayout>
-//     </div>
-//   )
-// }
-
-// export default AccountInfoPage
+import { IcCamera, IcChevronRight, IcMy } from '@/shared/assets/icon'
+import { ImgRoundGoogle, ImgRoundKakao } from '@/shared/assets/images'
+import { BackButton } from '@/shared/components/buttons/back-button'
+import { Header } from '@/shared/components/header'
+import { SystemDialog } from '@/shared/components/system-dialog'
+import { Input } from '@/shared/components/ui/input'
+import { Text } from '@/shared/components/ui/text'
+import { TextButton } from '@/shared/components/ui/text-button'
+import { RoutePath, useRouter } from '@/shared/lib/router'
 
 const AccountInfoPage = () => {
-  return null
+  const router = useRouter()
+
+  const { clearToken } = useAuthStore()
+  const { data: user } = useUser()
+
+  const [nameDialogOpen, setNameDialogOpen] = useState<boolean>(false)
+  const [logoutDialogOpen, setLogoutDialogOpen] = useState<boolean>(false)
+  const [newName, setNewName] = useState<string>('')
+  const [imageUrl, setImageUrl] = useState<string | null>(null)
+
+  const { mutate: updateMemberName, isPending } = useUpdateMemberName()
+
+  const interestCategories = useMemo(() => user && (user.category ? [user.category.id] : ['관심 분야 없음']), [user])
+
+  // user name이 변경될 경우 dialog input 기본 값도 변경
+  useEffect(() => {
+    if (user) {
+      setNewName(user.name)
+    }
+  }, [user])
+
+  // 로그아웃
+  const handleLogout = () => {
+    clearToken()
+  }
+
+  // 이름 변경 dialog open 핸들러
+  const handleSetNameDialogOpen = (open: boolean) => {
+    if (!open) {
+      setNewName(user?.name ?? '')
+    }
+
+    setNameDialogOpen(open)
+  }
+
+  // 이름 변경 핸들러
+  const handleChangeName = () => {
+    updateMemberName(
+      { name: newName },
+      {
+        onSuccess: () => {
+          toast('닉네임이 변경되었어요')
+          setNameDialogOpen(false)
+          setNewName(user?.name ?? '')
+        },
+      },
+    )
+  }
+
+  // File을 string으로 변환하는 함수
+  const convertFileToString = (file: File): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onload = () => resolve(reader.result as string)
+      reader.onerror = reject
+      reader.readAsDataURL(file)
+    })
+  }
+
+  // 파일 선택 핸들러
+  const handleChangeImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if (file) {
+      try {
+        // File을 string(Base64)으로 변환
+        const imageString = await convertFileToString(file)
+
+        // TODO: 이미지 설정 논의
+        // 여기서 서버에 이미지 업로드 로직을 구현 (서버에서 받는 형식에 따라 수정)
+        // uploadImageToServer(file);
+
+        // 성공 시
+        setImageUrl(imageString)
+        toast('프로필 사진이 변경되었어요')
+      } catch (error) {
+        console.error('이미지 변환 실패:', error)
+        toast('이미지 처리 중 오류가 발생했습니다')
+      }
+    }
+  }
+
+  return (
+    <div className="min-h-screen bg-base-1 flex flex-col">
+      <Header left={<BackButton />} title="계정 정보" />
+
+      <HeaderOffsetLayout className="w-full grow flex flex-col overflow-x-hidden px-[16px] justify-between">
+        <div className="w-full flex flex-col">
+          <div className="flex-center w-full pb-[44px] pt-[24px]">
+            <div className="relative">
+              <div className="flex-center relative size-[96px] overflow-hidden rounded-full bg-base-3">
+                {imageUrl ? (
+                  <img src={imageUrl} alt="" className="object-cover" />
+                ) : (
+                  <IcMy className="size-[56px] text-icon-sub" />
+                )}
+              </div>
+              {/* 이미지 등록 버튼 */}
+              <input
+                type="file"
+                name="file"
+                id="userImage"
+                className="hidden"
+                accept="image/*"
+                onChange={handleChangeImage}
+              />
+              <label
+                htmlFor="userImage"
+                className="flex-center absolute z-10 bottom-0 right-0 size-[32px] cursor-pointer rounded-full border border-outline bg-base-1"
+              >
+                <IcCamera className="size-[16px]" />
+              </label>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-[32px]">
+            <button
+              type="button"
+              onClick={() => setNameDialogOpen(true)}
+              className="flex w-full items-center justify-between"
+            >
+              <div className="flex flex-col items-start gap-[4px]">
+                <Text typo="body-1-medium" color="sub">
+                  닉네임
+                </Text>
+
+                <Text typo="subtitle-2-medium">{user?.name}</Text>
+              </div>
+              <IcChevronRight className="size-[16px] text-icon-sub" />
+            </button>
+            <SystemDialog
+              open={nameDialogOpen}
+              onOpenChange={handleSetNameDialogOpen}
+              title="닉네임 변경"
+              description="새로 사용할 이름을 입력해주세요"
+              preventClose={isPending}
+              content={
+                <>
+                  {isPending ? (
+                    <div className="animate-pulse text-center">Loading...</div>
+                  ) : (
+                    <Input
+                      value={newName}
+                      onChange={(e) => setNewName(e.target.value)}
+                      placeholder={user?.name}
+                      max={10}
+                      hasClear
+                      onClearClick={() => setNewName('')}
+                      helperText={`10자 이내로 입력해주세요 (${newName.length}/10)`}
+                    />
+                  )}
+                </>
+              }
+              onConfirm={handleChangeName}
+              disabledConfirm={newName.length === 0}
+            />
+
+            <InterestedCategoryDrawer interestedCategories={interestCategories as (number | '관심 분야 없음')[]} />
+
+            <div className="flex w-full items-center justify-between">
+              <div className="flex flex-col items-start gap-[4px]">
+                <Text typo="body-1-medium" color="sub">
+                  이메일
+                </Text>
+
+                <Text typo="subtitle-2-medium" color="primary">
+                  {user?.email ? user.email : '이메일 주소를 등록해주세요'}
+                </Text>
+              </div>
+            </div>
+
+            <div className="flex w-full items-center justify-between">
+              <div className="flex flex-col items-start gap-[4px]">
+                <Text typo="body-1-medium" color="sub">
+                  로그인 정보
+                </Text>
+                <div className="flex items-center gap-[8px]">
+                  {user?.socialPlatform === 'KAKAO' ? (
+                    <>
+                      <ImgRoundKakao className="size-[20px]" />
+                      <Text typo="subtitle-2-medium">카카오 로그인</Text>
+                    </>
+                  ) : (
+                    <>
+                      <ImgRoundGoogle className="size-[20px]" />
+                      <Text typo="subtitle-2-medium">구글 로그인</Text>
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-center pb-[56px]">
+          <TextButton
+            onClick={() => setLogoutDialogOpen(true)}
+            variant={'sub'}
+            size={'sm'}
+            className="pr-[16px] border-r border-divider"
+          >
+            로그아웃
+          </TextButton>
+          <SystemDialog
+            open={logoutDialogOpen}
+            onOpenChange={setLogoutDialogOpen}
+            title="로그아웃 하시겠어요?"
+            cancelLabel="취소"
+            confirmLabel="로그아웃"
+            variant="critical"
+            onConfirm={handleLogout}
+          />
+          <TextButton
+            onClick={() => router.push(RoutePath.accountWithdraw)}
+            variant={'sub'}
+            size={'sm'}
+            className="pl-[16px]"
+          >
+            탈퇴하기
+          </TextButton>
+        </div>
+      </HeaderOffsetLayout>
+    </div>
+  )
 }
 
 export default AccountInfoPage
+
+// const AccountInfoPage = () => {
+//   return null
+// }
+
+// export default AccountInfoPage
--- src/pages/account/account-page.tsx
+++ src/pages/account/account-page.tsx
@@ -1,7 +1,231 @@
 import { withHOC } from '@/app/hoc/with-page-config'
+import HeaderOffsetLayout from '@/app/layout/header-offset-layout'
+
+import { IcChevronRight, IcDisclaimer, IcMy, IcNotification, IcRecord } from '@/shared/assets/icon'
+import { ImgGraph, ImgInviteStar, ImgStar } from '@/shared/assets/images'
+import { BackButton } from '@/shared/components/buttons/back-button'
+import { Header } from '@/shared/components/header'
+import { Button } from '@/shared/components/ui/button'
+import { Disclaimer, DisclaimerContent, DisclaimerTrigger } from '@/shared/components/ui/disclaimer'
+import { Switch } from '@/shared/components/ui/switch'
+import { Tag } from '@/shared/components/ui/tag'
+import { Text } from '@/shared/components/ui/text'
+import { Link, useRouter } from '@/shared/lib/router'
 
 const AccountPage = () => {
-  return <div>AccountPage</div>
+  const router = useRouter()
+
+  return (
+    <>
+      <Header
+        left={<BackButton type="close" />}
+        title={'MY'}
+        right={
+          <div className="flex items-center">
+            <button type="button" className="size-[40px] flex-center">
+              <IcRecord className="size-[24px]" />
+            </button>
+            <button type="button" className="size-[40px] flex-center">
+              <IcNotification className="size-[24px]" />
+            </button>
+          </div>
+        }
+      />
+
+      <HeaderOffsetLayout className="h-full overflow-y-auto scrollbar-hide">
+        <div className="px-[16px] pb-[36px] flex flex-col">
+          <div className="flex items-center gap-[16px] py-[20px]">
+            <div className="size-[72px] bg-base-3 rounded-full flex-center shrink-0">
+              <IcMy className="size-[42px] text-icon-sub" />
+            </div>
+
+            <div className="w-[calc(100%-72px-16px)] flex flex-col gap-[4px]">
+              <div className="w-full flex items-center gap-[8px]">
+                <Text typo="subtitle-1-bold" className="min-w-[47px] max-w-[70%] truncate">
+                  {'픽토스'}
+                </Text>
+                <Tag className="shrink-0">{'🎓  학문·전공'}</Tag>
+              </div>
+
+              <Text typo="body-1-medium" color="sub">
+                picktoss@gmail.com
+              </Text>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-[24px]">
+            <div className="bg-surface-2 rounded-[12px] flex items-center h-[56px] py-[12px]">
+              <div className="flex-1/2 h-full px-[16px] flex-center gap-[8px] border-r border-divider">
+                <Text typo="subtitle-2-bold" color="secondary">
+                  내 퀴즈
+                </Text>
+                <Text typo="subtitle-2-bold" color="accent">
+                  {13}
+                </Text>
+              </div>
+              <div className="flex-1/2 h-full px-[16px] flex-center gap-[8px]">
+                <Text typo="subtitle-2-bold" color="secondary">
+                  북마크
+                </Text>
+                <Text typo="subtitle-2-bold" color="accent">
+                  {23}
+                </Text>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-[12px]">
+              <div className="flex items-center justify-between border border-outline rounded-[12px] h-[80px] py-[16px] px-[20px]">
+                <div className="flex items-center gap-[12px]">
+                  <ImgGraph width={40} height={40} />
+
+                  <div className="flex flex-col">
+                    <Text typo="body-2-medium" color="sub">
+                      이번 달 푼 퀴즈
+                    </Text>
+                    <Text typo="subtitle-1-bold">{135} 문제</Text>
+                  </div>
+                </div>
+
+                <Button onClick={() => router.push('/account/quiz-analysis')} size={'sm'} variant={'tertiary'}>
+                  분석 보기
+                </Button>
+              </div>
+
+              <div className="flex flex-col gap-[21px] border border-outline rounded-[12px] p-[16px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[4px]">
+                    <ImgStar width={24} height={24} />
+                    <Text typo="subtitle-2-bold">나의 별</Text>
+                    <Disclaimer>
+                      <DisclaimerTrigger asChild>
+                        <IcDisclaimer className="size-[16px] text-icon-sub" />
+                      </DisclaimerTrigger>
+                      <DisclaimerContent side="bottom" sideOffset={7} align="start" alignOffset={-7}>
+                        퀴즈를 생성하기 위해 필요한 재화로, 매일 <br /> 데일리에서 10문제 이상 풀거나 이벤트에 <br />{' '}
+                        참여하면 받을 수 있어요
+                      </DisclaimerContent>
+                    </Disclaimer>
+                  </div>
+
+                  <Link to="/account/my-star" type="button" className="flex items-center gap-[4px]">
+                    <Text typo="subtitle-2-bold">{130}개</Text>
+                    <IcChevronRight className="size-[16px] text-icon-sub" />
+                  </Link>
+                </div>
+
+                <div className="bg-accent rounded-[8px] py-[16px] px-[24px] flex items-center gap-[16px]">
+                  <div className="flex-1 flex flex-col gap-[4px]">
+                    <Text typo="body-1-medium" color="secondary">
+                      픽토스 초대장 보내기
+                    </Text>
+                    <Text typo="subtitle-1-bold">
+                      초대할 때마다{' '}
+                      <Text as={'span'} typo="subtitle-1-bold" color="accent">
+                        별 50개!
+                      </Text>
+                    </Text>
+                  </div>
+
+                  <ImgInviteStar width={56} height={56} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-[8px] w-full bg-base-2"></div>
+
+        <div className="py-[24px] px-[16px] flex flex-col gap-[24px]">
+          <div className="flex flex-col gap-[16px]">
+            <Text typo="body-1-medium" color="caption">
+              설정
+            </Text>
+            <div className="flex flex-col gap-[20px]">
+              <Link to="/account/info" className="flex items-center justify-between">
+                <Text typo="subtitle-2-medium">계정 정보</Text>
+                <IcChevronRight className="size-[16px] text-icon-sub" />
+              </Link>
+
+              <div className="flex items-center justify-between">
+                <Text typo="subtitle-2-medium">푸시 알림</Text>
+                <Switch size="md" />
+              </div>
+            </div>
+          </div>
+
+          <div className="border-t border-divider pt-[16px] flex flex-col gap-[16px]">
+            <Text typo="body-1-medium" color="caption">
+              도움말 & 지원
+            </Text>
+            <div className="flex flex-col gap-[20px]">
+              <Link to="/account/feedback" className="flex items-center justify-between">
+                <Text typo="subtitle-2-medium">문의하기</Text>
+                <IcChevronRight className="size-[16px] text-icon-sub" />
+              </Link>
+
+              <a
+                href="https://picktoss.notion.site/1209d818f5608088a977c9ee5f70061f"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center justify-between"
+              >
+                <Text typo="subtitle-2-medium">이용 가이드</Text>
+                <IcChevronRight className="size-[16px] text-icon-sub" />
+              </a>
+
+              <Link to="/account/notice" className="flex items-center justify-between">
+                <Text typo="subtitle-2-medium">공지사항</Text>
+                <IcChevronRight className="size-[16px] text-icon-sub" />
+              </Link>
+
+              <a
+                href="https://picktoss.framer.website"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center justify-between"
+              >
+                <Text typo="subtitle-2-medium">서비스 소개</Text>
+                <IcChevronRight className="size-[16px] text-icon-sub" />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <footer className="bg-surface-2 pt-[24px] px-[16px] pb-[40px] flex flex-col gap-[20px]">
+          <div className="flex items-center gap-[32px]">
+            <a
+              href="https://picktoss.notion.site/1209d818f56080fbb469e82def758e9c"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Text typo="body-1-medium" color="sub">
+                개인정보 처리방침
+              </Text>
+            </a>
+            <a
+              href="https://picktoss.notion.site/1209d818f560809aad11c5b64020d735"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Text typo="body-1-medium" color="sub">
+                서비스 이용약관
+              </Text>
+            </a>
+          </div>
+
+          <div className="flex flex-col gap-[16px]">
+            <Text typo="body-2-medium" color="caption">
+              사업자등록번호 120-20-02237 | 070-7954-1774 | 대표자명 : 이유민 <br /> 서울특별시 서대문구 연희로41가길 5,
+              B1층 D72호(홍은동)
+            </Text>
+            <Text typo="body-2-medium" color="caption">
+              ⓒ 2024. picktoss all rights reserved
+            </Text>
+          </div>
+        </footer>
+      </HeaderOffsetLayout>
+    </>
+  )
 }
 
 export default withHOC(AccountPage, {})
