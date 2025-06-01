d1707ecf62ceab83931bdd2b63a1bd2912ffaad7
류정우 <evencoding@gmail.com>
2025-06-01 19:43:33 +0900

    web flow

--- src/app/layout/pwa-only-mobile-layout.tsx
+++ src/app/layout/pwa-only-mobile-layout.tsx
@@ -15,8 +15,8 @@ export const PWAOnlyMobileLayout = () => {
         {/* TODO: 탐험 탭 페이지가 보여야 함 */}
 
         <div>모바일 픽토스는 앱에서 만날 수 있어요</div>
-        <Button onClick={() => router.replace('/explore')}>컬렉션으로 이동하기</Button>
-        <Button onClick={() => router.replace('/install-guide')}>지금 앱에서 전부 이용하기</Button>
+        <Button onClick={() => router.push('/explore')}>탐험으로 이동하기</Button>
+        <Button onClick={() => router.push('/install-guide')}>지금 앱에서 전부 이용하기</Button>
       </div>
     )
   }
