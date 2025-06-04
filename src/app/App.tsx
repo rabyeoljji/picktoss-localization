28b85d299ba836f5088f6815343dab9c589bec68
jw-r <evencoding@gmail.com>
2025-06-05 06:36:54 +0900

    feat: main og

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,32 @@
+import { Helmet } from 'react-helmet-async'
+
 import { AppRouter } from '@/app/app-router'
 import { Providers } from '@/app/providers'
 
 function App() {
   return (
     <Providers>
+      <Helmet>
+        <title>픽토스: 나를 성장시키는 AI 퀴즈</title>
+        <meta name="description" content="나를 성장시키는 똑똑한 퀴즈" />
+
+        {/* OpenGraph meta tags */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://picktoss.com" />
+        <meta property="og:title" content="픽토스: 나를 성장시키는 AI 퀴즈" />
+        <meta property="og:description" content="나를 성장시키는 똑똑한 퀴즈" />
+        <meta property="og:image" content="/images/og-image.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        {/* Twitter Card meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="픽토스: 나를 성장시키는 AI 퀴즈" />
+        <meta name="twitter:description" content="나를 성장시키는 똑똑한 퀴즈" />
+        <meta name="twitter:image" content="/images/og-image.png" />
+
+        <link rel="canonical" href="https://www.picktoss.com" />
+      </Helmet>
       <AppRouter />
     </Providers>
   )
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,3 +1,5 @@
+import { HelmetProvider } from 'react-helmet-async'
+
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
@@ -9,13 +11,15 @@ const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || ''
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <AmplitudeProvider>
-      <QueryClientProvider client={queryClient}>
-        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-          {children}
-          <Toaster expand position="bottom-center" duration={3000} style={{ marginBottom: '40px' }} />
-        </GoogleOAuthProvider>
-      </QueryClientProvider>
-    </AmplitudeProvider>
+    <HelmetProvider>
+      <AmplitudeProvider>
+        <QueryClientProvider client={queryClient}>
+          <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+            {children}
+            <Toaster expand position="bottom-center" duration={3000} style={{ marginBottom: '40px' }} />
+          </GoogleOAuthProvider>
+        </QueryClientProvider>
+      </AmplitudeProvider>
+    </HelmetProvider>
   )
 }
