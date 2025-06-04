4ecbe8728130993f23d89d4cae9b2b0ee42c0bc1
jw-r <evencoding@gmail.com>
2025-06-05 06:47:39 +0900

    feat: og

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,25 +12,25 @@ function App() {
 
         {/* OpenGraph meta tags */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://picktoss.com" />
+        <meta property="og:url" content="https://picktoss.vercel.app" />
         <meta property="og:title" content="픽토스: 나를 성장시키는 AI 퀴즈" />
-        {/* <meta property="og:description" content="나를 성장시키는 똑똑한 퀴즈" /> */}
-        <meta property="og:image" content="/images/og-image.png" />
+        <meta property="og:description" content="나를 성장시키는 똑똑한 퀴즈" />
+        <meta property="og:image" content="https://picktoss.vercel.app/images/og-image.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
 
         {/* Twitter Card meta tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="픽토스: 나를 성장시키는 AI 퀴즈" />
-        {/* <meta name="twitter:description" content="나를 성장시키는 똑똑한 퀴즈" /> */}
-        <meta name="twitter:image" content="/images/og-image.png" />
+        <meta name="twitter:description" content="나를 성장시키는 똑똑한 퀴즈" />
+        <meta name="twitter:image" content="https://picktoss.vercel.app/images/og-image.png" />
 
         {/* Favicon */}
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/favicon.svg" />
         <link rel="mask-icon" href="/favicon.svg" color="#FF9928" />
 
-        <link rel="canonical" href="https://www.picktoss.com" />
+        <link rel="canonical" href="https://www.picktoss.vercel.app" />
       </Helmet>
       <AppRouter />
     </Providers>
