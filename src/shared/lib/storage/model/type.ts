c88bb3d62487dfe654334488ec119f43df211070
jw-r <evencoding@gmail.com>
2025-06-12 16:48:40 +0900

    fix: build error

--- src/shared/lib/storage/model/type.ts
+++ src/shared/lib/storage/model/type.ts
@@ -4,10 +4,10 @@ import type { StorageKey, StorageSchema } from '../config'
  * 스토리지 키 타입
  * 모든 스토리지 키 문자열 리터럴의 유니온 타입
  */
-export type StorageKeyType = keyof typeof StorageKey
+export type StorageKeyType = keyof typeof StorageKey & keyof StorageSchema
 
 /**
  * 스토리지 값 타입
  * 스토리지 키를 사용하여 해당 값의 타입을 조회
  */
-export type StorageValue<K extends StorageKeyType> = K extends keyof StorageSchema ? StorageSchema[K] : never
+export type StorageValue<K extends StorageKeyType> = StorageSchema[K]
