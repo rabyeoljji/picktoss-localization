1c3af6f1e39acdc406779feaa843993beb1c485b
nageuna <nageuna922@gmail.com>
2025-06-16 22:10:25 +0900

    qa: mobile web view layout

--- src/app/layout/root-layout.tsx
+++ src/app/layout/root-layout.tsx
@@ -1,10 +1,23 @@
 import { Suspense } from 'react'
+import { isMobile } from 'react-device-detect'
 import { ErrorBoundary } from 'react-error-boundary'
 import { Outlet } from 'react-router'
 
+import { cn } from '@/shared/lib/utils'
+
 export const RootLayout = () => {
+  const checkPWA = () => {
+    const isIOSStandalone =
+      typeof window !== 'undefined' && 'standalone' in window.navigator && window.navigator.standalone
+    const isStandaloneMedia = window.matchMedia('(display-mode: standalone)').matches
+
+    return isIOSStandalone || isStandaloneMedia
+  }
+
+  const accessMobileWeb = !checkPWA() && isMobile
+
   return (
-    <div className="mx-auto h-screen max-w-xl bg-gray-50 overscroll-none">
+    <div className={cn('mx-auto h-screen max-w-xl bg-gray-50 overscroll-none', accessMobileWeb && 'h-dvh')}>
       <ErrorBoundary
         fallbackRender={(e) => {
           console.error(e)
