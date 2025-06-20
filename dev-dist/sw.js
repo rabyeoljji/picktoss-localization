48d095f575cfb2378efbfdb487a2225eba0fdbbe
jw-r <evencoding@gmail.com>
2025-06-20 15:03:39 +0900

    feat: install quide

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hfqnoa44hgg"
+    "revision": "0.9md5evu7p3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/install-guide-page.tsx
+++ src/pages/install-guide-page.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { isIOS } from 'react-device-detect'
 
 import { IcLogo } from '@/shared/assets/icon'
@@ -47,6 +48,28 @@ interface AppInstallAosProps {
 }
 
 const AppInstallAos = ({ handleInstallClick }: AppInstallAosProps) => {
+  const [showInstallGuide, setShowInstallGuide] = useState(false)
+
+  if (showInstallGuide) {
+    return (
+      <main className="flex h-dvh flex-col items-center overflow-y-auto bg-base-02 px-[16px] pb-[112px] pt-[52px]">
+        <Text typo="h2" color="secondary" className="text-center">
+          <span className="text-caption">스토어 방문 없이</span>
+          <br />
+          3초만에 시작할 수 있어요
+        </Text>
+
+        <div className="mt-[49px] flex flex-col gap-4 *:w-full *:max-w-[380px]">
+          <img src="/images/ios-guide-1.png" alt="" />
+
+          <img src="/images/ios-guide-2.png" alt="" />
+
+          <img src="/images/ios-guide-3.png" alt="" />
+        </div>
+      </main>
+    )
+  }
+
   return (
     <main className="flex min-h-dvh flex-col flex-center overflow-y-auto bg-base-02 px-[16px] py-[32px]">
       <Text typo="h2" color="secondary" className="text-center">
@@ -63,6 +86,12 @@ const AppInstallAos = ({ handleInstallClick }: AppInstallAosProps) => {
           앱 다운로드
         </Button>
       </div>
+
+      <button onClick={() => setShowInstallGuide(true)}>
+        <Text typo="body-1-medium" color="secondary" className="text-center underline mt-[32px] underline-offset-2">
+          혹시 다운로드가 되지 않는다면?
+        </Text>
+      </button>
     </main>
   )
 }
