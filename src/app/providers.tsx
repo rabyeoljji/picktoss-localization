db60b396a3d4c622e09558014cfc7d2808330d52
류정우 <evencoding@gmail.com>
2025-06-29 20:01:16 +0900

    fix: build error

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,3 @@
-import { HelmetProvider } from 'react-helmet-async'
-
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
@@ -11,15 +9,13 @@ const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || ''
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <HelmetProvider>
-      <AmplitudeProvider>
-        <QueryClientProvider client={queryClient}>
-          <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-            {children}
-            <Toaster expand position="bottom-center" duration={3000} style={{ marginBottom: '40px' }} />
-          </GoogleOAuthProvider>
-        </QueryClientProvider>
-      </AmplitudeProvider>
-    </HelmetProvider>
+    <AmplitudeProvider>
+      <QueryClientProvider client={queryClient}>
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+          {children}
+          <Toaster expand position="bottom-center" duration={3000} style={{ marginBottom: '40px' }} />
+        </GoogleOAuthProvider>
+      </QueryClientProvider>
+    </AmplitudeProvider>
   )
 }
