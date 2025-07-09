b025ac6a3064bfa7a4b117378defa45361680d1c
류정우 <evencoding@gmail.com>
2025-07-09 21:32:43 +0900

    feat: clipboard

--- src/features/note/ui/note-create-write/index.tsx
+++ src/features/note/ui/note-create-write/index.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 import { useKeyboard } from '@/app/keyboard-detector'
 
-import { IcInfo } from '@/shared/assets/icon'
+import { IcCopy, IcInfo } from '@/shared/assets/icon'
+import { Button } from '@/shared/components/ui/button'
 import { Text } from '@/shared/components/ui/text'
 import { Textarea } from '@/shared/components/ui/textarea'
 import { usePWA } from '@/shared/hooks/use-pwa'
@@ -17,6 +18,32 @@ export const NoteCreateWrite = () => {
   const { isKeyboardVisible } = useKeyboard()
   const [textareaHeight, setTextareaHeight] = useState(300)
   const { isPWA } = usePWA()
+
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+  const [canPaste, setCanPaste] = useState(false)
+
+  const handlePointerDown = async () => {
+    if (!navigator.clipboard) return
+    try {
+      const text = await navigator.clipboard.readText() // 동기(=제스처) 컨텍스트 유지
+      setCanPaste(!!text)
+    } catch {
+      setCanPaste(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('pointerdown', handlePointerDown)
+    return () => window.removeEventListener('pointerdown', handlePointerDown)
+  }, [])
+
+  const handlePasteClick = async () => {
+    if (!textAreaRef.current) return
+    const text = await navigator.clipboard.readText()
+    textAreaRef.current.value = text
+    setContent(text)
+  }
 
   const handleTextareaChange = (content: string) => {
     setContent(content)
@@ -48,6 +75,7 @@ export const NoteCreateWrite = () => {
   return (
     <div className="flex-1 relative">
       <Textarea
+        ref={textAreaRef}
         placeholder="여기를 탭하여 입력을 시작하세요"
         className={cn('border-none px-4', isPWA && 'pb-[env(safe-area-inset-top)]')}
         onChange={(e) => handleTextareaChange(e.target.value)}
@@ -55,6 +83,17 @@ export const NoteCreateWrite = () => {
           height: textareaHeight,
         }}
       />
+      {canPaste && content.length === 0 && (
+        <Button
+          variant="secondary2"
+          size="sm"
+          left={<IcCopy />}
+          onClick={handlePasteClick}
+          className="absolute left-[16px] top-[77px]"
+        >
+          복사한 텍스트 붙여넣기
+        </Button>
+      )}
 
       {isKeyboardVisible && (
         <div className="flex justify-between bg-base-1 items-center px-4 h-[40px] border-t border-divider absolute bottom-0 w-full max-w-xl">
